df52594d8d05bd7b0a57b3cbecfa8a0ad3397ac8
JKLEE <hhth999@naver.com>
2020-04-28 13:53:14 +0900

    라이브러리 사용하기

--- greeting.js
+++ greeting.js
@@ -1,3 +1,23 @@
 function welcome() {
   return "Hello World";
 }
+
+let con = {
+  egoing: 80,
+  billion: true,
+  test: 10 * 2,
+  str: "Welcome",
+  func: function () {
+    alert("Test");
+    let func2 = {
+      cons: function () {
+        return this.str;
+      },
+    };
+  },
+  obj: {
+    test1: "Test1",
+    test2: "test2 ",
+    test3: "test3",
+  },
+};
